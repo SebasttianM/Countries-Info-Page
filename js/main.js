18454cdb6196316362fcd61285a10d32d46a6944
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-09 11:30:05 -0500

    modal incluido no renderiza bien los paises en el modal

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@ import getData from './getData.js';
 import showData from './showData.js';
 
 const endpoint = 'https://restcountries.com/v3.1/all';
-const americas = document.getElementById('Americas')
-const africa = document.getElementById('Africa')
-const asia = document.getElementById('Asia')
-const europe = document.getElementById('Europe')
-const oceania = document.getElementById('Oceania')
+const americas = document.getElementById('Americas');
+const africa = document.getElementById('Africa');
+const asia = document.getElementById('Asia');
+const europe = document.getElementById('Europe');
+const oceania = document.getElementById('Oceania');
 
-const urlamericas ='https://restcountries.com/v3.1/region/americas'
+const urlamericas = 'https://restcountries.com/v3.1/region/americas';
 
-const searchRegion= 'https://restcountries.com/v3.1/region/'
+const searchRegion = 'https://restcountries.com/v3.1/region/';
 
 const card = document.querySelector('#card');
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (e) => {
+  e.preventDefault();
   const data = getData(endpoint);
   showData(data, card);
 });
 
-
-const search= (region,endpoint)=>{
-  region.addEventListener('click',()=>{
-    const dataRegion = getData(searchRegion+endpoint)
-    showData(dataRegion, card)
-  })
-}
-search(americas,"americas")
-search(africa,"africa")
-search(asia,"asia")
-search(europe,"europe")
-search(oceania,"oceania")
+const search = (region, endpoint) => {
+  region.addEventListener('click', () => {
+    const dataRegion = getData(searchRegion + endpoint);
+    showData(dataRegion, card);
+  });
+};
+search(americas, 'americas');
+search(africa, 'africa');
+search(asia, 'asia');
+search(europe, 'europe');
+search(oceania, 'oceania');
--- js/showData.js
+++ js/showData.js
@@ -2,25 +2,37 @@ const showData = async (country, card) => {
   card.innerHTML = '';
   const allCountries = await country;
   allCountries.forEach((info) => {
-    const { name, flags, cca2 } = info;
+    const { name, flags, cca2, capital, region, flag } = info;
     card.innerHTML += `
 
     <div class="card">
-    <img src=${flags.png} alt="" />
-    <div class="botones">
-      <a href="#" class="boton primario"><small>${cca2}</small> - <strong>${name.common}</strong></a>
-    </div>
-  </div>
+          <img src=${flags.png} alt="" />
+          <div class="botones boton-modal">
+            <label for="btn-modal" class="boton primario"><small>${cca2}</small> - <strong>${name.common}</strong>  </label>
+          </div>
+
+        
+          <input type="checkbox" id="btn-modal" />
+          <div class="container-modal">
+            <div class="content-modal">
+              <h2>${flag} - ${name.official} </h2>
+              <p>
+               <strong>Capital:</strong> ${capital}
+               </p>
+               <p>
+               <strong>Continent :</strong> ${region}
+               </p>
+               
+              <div class="btn-cerrar">
+                <label for="btn-modal">Cerrar</label>
+              </div>
+            </div>
+            <label for="btn-modal" class="cerrar-modal"></label>
+          </div>
+        </div>
+  
     `;
   });
 };
 
-
-
-
-
-
-
-
 export default showData;
-
