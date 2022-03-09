a2d92f0fad49fb6f1f9fb7849a868d56b064eb41
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-08 21:32:46 -0500

    agregamos getdata showdata y main accediendo a la api

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import getData from './getData.js';
+import showData from './showData.js';
+
+const endpoint = 'https://restcountries.com/v3.1/all';
+
+const card = document.querySelector('#card');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const data = getData(endpoint);
+  showData(data, card);
+});
